efeee32c33e9f16098cccced887bf0b33c3568b4
Subrat <subrat0207@gmail.com>
2022-12-05 06:06:35 +0530

    adding step for main program execution in gitaction workflow file

--- main_program.py
+++ main_program.py
@@ -1,14 +1,14 @@
 
-from diff.sk_diff import sk_diff_1
+from diff.sk_diff import math_1
 
-from summ.sk_sum import sk_sum_1
+from summ.sk_sum import math_2
 
 def cal():
     a = 10 
     b = 5
 
-    sum_1= sk_sum_1(a,b)
-    dif_1 = sk_diff_1(a,b)
+    sum_1= math_2.sk_sum_1(a,b)
+    dif_1 = math_1.sk_diff_1(a,b)
 
     print("The sum is :",sum_1)
     print("The diff is :",dif_1)
