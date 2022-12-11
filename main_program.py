4130b39b545650684c20503917a89ab27e3855c9
Subrat <subrat0207@gmail.com>
2022-12-11 16:18:43 +0530

    Pydeantic EXP

--- main_program.py
+++ main_program.py
@@ -1,19 +1,52 @@
+from pydantic import BaseModel
+from typing import Optional
 
-from diff.sk_diff import math_1
 
-from summ.sk_sum import math_2
+def clean_str_int(str_val):
+    if bool(str_val):return '1'
+    else:pass
 
-def cal():
-    a = 10 
-    b = 5
+def clean_str_bool(str_val):
+    if bool(str_val):return 'y'
+    else:pass
 
-    sum_1= math_2.sk_sum_1(a,b)
-    dif_1 = math_1.sk_diff_1(a,b)
+class User(BaseModel):
+    id:int
+    user_name:str
+    pass_word:Optional[str]
+    test:Optional[bool]
 
-    print("The sum is :",sum_1)
-    print("The diff is :",dif_1)
 
+
+data = {'id':'1234','user_name':"Subrat",'pass_word':'kumar','test':'y'}
+
+
+data_1 = {'id':clean_str_int('Subrat Kumar Das'),'user_name':"Subrat",'pass_word':'kumar','test':'n'}
+
+data_2 = {'id':clean_str_int('Subrat Kumar Das'),'user_name':"Subrat",'pass_word':'kumar','test':clean_str_bool('LOW')}
+
+data_3 = {'id':clean_str_int('Subrat Kumar Das'),'user_name':"Subrat",'pass_word':'kumar','test':clean_str_bool('')}
+data_4 = {'id':clean_str_int('Subrat Kumar Das'),'user_name':"Subrat",'pass_word':'kumar','test':'N'}
+
+def timer_decorator(func):
+    def timer_wrapper(*args, **kwargs):
+        import datetime                 
+        before = datetime.datetime.now()
+        #print(before)                     
+        result = func(*args,**kwargs)                
+        after = datetime.datetime.now()
+        #print(after)                        
+        print("Elapsed Time = {0}".format(after-before) )     
+    return timer_wrapper
+    
+@timer_decorator
+def sum(x, y):
+    print(x + y)
+    return x + y
 
 if __name__ == "__main__":
     print('Program Starts:')
-    cal()
+    user = User(**data_4)
+    k = user
+    print(k)
+
